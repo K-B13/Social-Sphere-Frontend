3db8c1c53e369a34b4b0438f360050ebea462f42
Cezary_Karwoski-Budd <cezary.karwoski-budd@hotmail.com>
2023-06-03 21:48:00 +0100

    setting up beginnings of other users profiles

--- src/components/mainsite/ProfilePage.js
+++ src/components/mainsite/ProfilePage.js
@@ -1,14 +1,43 @@
 import { useState, useEffect } from "react"
+import { useParams, useNavigate } from "react-router"
+import { getUser } from "../../api/UserApis"
+import UserInformation from "./UserInformation"
+import FriendList from "./FriendList"
+import { loadUserData } from "../../helperFunctions/Helper"
+import { createRequest } from "../../api/FriendRequestApis"
 
 export default function ProfilePage() {
   const [ userInformation, setUserInformations ] = useState({})
-
+  const { userId } = useParams()
+  const navigate = useNavigate()
   useEffect(() => {
-    
-  })
+    getUser(loadUserData().id, userId)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(loadUserData().id)
+      console.log(data)
+      if (data.user.username === loadUserData().username) navigate('/MyProfile')
+      else setUserInformations(data)})
+  }, [])
+
+  const sendARequest =() => {
+    createRequest(loadUserData().id, userId)
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+  }
 
   return(
-    <p>Testing</p>
+    <div>
+      {userInformation.user ? <div><UserInformation 
+      userDetails={userInformation.user}
+      />
+      <FriendList /> </div> : null}
+      {!userInformation.isFriend && !userInformation.hasFriendRequest && 
+      <button
+      onClick={sendARequest()}
+      >Add Friend</button>
+      }
+    </div>
   )
 
 }
